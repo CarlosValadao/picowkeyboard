7839c27dea97ceb0147a791677bdad0ad988e2c3
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-01-17 12:08:19 -0300

    Biblioteca rgb

---
+++ rgb.c
@@ -0,0 +1,41 @@
+#include "rgb.h"
+
+void rgb_init(uint red, uint green, uint blue)
+{
+    return;
+}
+
+void rgb_turn_on_red(uint red, uint green, uint blue)
+{
+    return;
+}
+
+void rgb_turn_off_red(uint red, uint green, uint blue)
+{
+    return;
+}
+
+void rgb_turn_on_green(uint red, uint green, uint blue)
+{
+    return;
+}
+
+void rgb_turn_off_green(uint red, uint green, uint blue)
+{
+    return;
+}
+
+void rgb_turn_on_blue(uint red, uint green, uint blue)
+{
+    return;
+}
+
+void rgb_turn_off_blue(uint red, uint green, uint blue)
+{
+    return;
+}
+
+void rgb_blink(uint red, uint green, uint blue)
+{
+    return;
+}
---
+++ rgb.h
@@ -0,0 +1,10 @@
+#include "pico/stdlib.h"
+
+void rgb_init(uint red, uint green, uint blue);
+void rgb_turn_on_red(uint red, uint green, uint blue);
+void rgb_turn_off_red(uint red, uint green, uint blue);
+void rgb_turn_on_green(uint red, uint green, uint blue);
+void rgb_turn_off_green(uint red, uint green, uint blue);
+void rgb_turn_on_blue(uint red, uint green, uint blue);
+void rgb_turn_off_blue(uint red, uint green, uint blue);
+void rgb_blink(uint red, uint green, uint blue);
