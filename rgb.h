3e041ac8a642663872e9da6240a0f457827d2bf3
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-01-17 12:20:37 -0300

    Adicionar documentação

--- rgb.h
+++ rgb.h
@@ -1,4 +1,16 @@
+#ifndef RGB_H
+#define RGB_H
+
 #include "pico/stdlib.h"
+
+/** 
+ * @file rgb.h
+ * @brief Este arquivo contém declarações de funções e definições relacionadas a um
+ *        LED RGB conectado aos pinos GPIO
+ *
+ * @author Carlos Valadao
+ * @date 17/01/2025
+ */
 
 void rgb_init(uint red, uint green, uint blue);
 void rgb_turn_on_red(uint red, uint green, uint blue);
@@ -8,3 +20,5 @@ void rgb_turn_off_green(uint red, uint green, uint blue);
 void rgb_turn_on_blue(uint red, uint green, uint blue);
 void rgb_turn_off_blue(uint red, uint green, uint blue);
 void rgb_blink(uint red, uint green, uint blue);
+
+#endif
